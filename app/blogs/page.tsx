27cd3218e24d8c5acb64817956389da448867e03
Mili srivastava <milisrivastava42@gmail.com>
2024-03-01 19:59:56 +0530

    added link to blogs

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -8,54 +8,63 @@ const page = () => {
         title="Asynchronous Flow Control Techniques"
         description="Let's dive into Asynchronous Flow and let us understand what techniques we can use to do asynchrounous programming in JavaScript, so let's simplify it and understand how it works together!"
         image="/images/blog3.png"
+        link="https://www.linkedin.com/pulse/asynchronous-flow-control-techniques-mili-srivastava-sdftf/?trackingId=7LOKFu2VTWypQplvL4mxDA%3D%3D"
       />
 
       <Blog
         title="Asynchrounous Flow Control And Event Loop"
         description="Let's dive into Asynchronous Flow and the Event Loop in JavaScript! 🚀 They're the keys to making code efficient and responsive. I'm learning too, so let's simplify it and understand how it works together!"
         image="/images/blog1.png"
+        link="https://www.linkedin.com/pulse/asynchronous-flow-control-event-loop-mili-srivastava/?trackingId=9NH6wsaPSvywRiwcAiXFXA%3D%3D"
       />
 
       <Blog
         title="REST API"
         description="We've all heard of APIs, those nifty tools that make our lives easier. One of them, REST API, is a game-changer for simplifying and scaling our projects. In this article, let's dive into #RESTAPI in plain and simple language."
         image="/images/blog2.jpg"
+        link="https://www.linkedin.com/pulse/rest-api-mili-srivastava/"
       />
 
       <Blog
         title="What is Node js and How it works?"
         description="Let's dive into Node js and discuss what Node Js is and the architecture of Node Js, so let's simplify it and understand how it works together!"
         image="/images/blog4.png"
+        link="https://www.linkedin.com/pulse/what-node-js-how-works-mili-srivastava/"
       />
 
       <Blog
         title="Monorepo vs. Polyrepo and How CI/CD Pipelines Work in Both Cases?"
         description="In this blog let's discuss about Monorepo and Polyrepo and how CI/CD pipelines work in both cases."
         image="/images/blog5.jpg"
+        link="https://www.linkedin.com/pulse/monorepo-vs-polyrepo-how-cicd-pipelines-work-both-case-srivastava/?trackingId=9NH6wsaPSvywRiwcAiXFXA%3D%3D"
       />
 
       <Blog
         title="CI/CD Pipelines for Microservices"
         description="CI/CD pipelines for microservices automate integration, and deployment. They ensure continuous delivery, scalability, and reliability, facilitating rapid development microservices architecture."
         image="/images/blog6.png"
+        link="https://www.linkedin.com/pulse/cicd-pipelines-microservices-mili-srivastava/?trackingId=9NH6wsaPSvywRiwcAiXFXA%3D%3D"
       />
 
       <Blog
         title="Monolithic Architecture Vs Microservices"
         description="Microservices is the way of software development where software is composed of small components also known as services that communicate through APIs."
         image="/images/blog7.png"
+        link="https://www.linkedin.com/pulse/monolithic-architecture-vs-microservices-mili-srivastava/?trackingId=9NH6wsaPSvywRiwcAiXFXA%3D%3D"
       />
 
       <Blog
         title="Static And Dynamic Websites"
         description="Static websites present fixed content, best for showcasing information. Dynamic websites offer interactive elements, changing content based on user input, ideal for engaging blogs and real-time updates."
         image="/images/blog8.jpg"
+        link="https://www.linkedin.com/pulse/static-dynamic-websites-mili-srivastava/?trackingId=9NH6wsaPSvywRiwcAiXFXA%3D%3D"
       />
 
       <Blog
         title="Understanding the Client-Server Architecture"
         description="In this blog we'll discuss in detail about Client-Server architecture, Web Server and types of HTTP methods, so let's simplify it and understand how it works together!"
         image="/images/blog9.jpg"
+        link="https://www.linkedin.com/pulse/understanding-client-server-architecture-mili-srivastava/?trackingId=9NH6wsaPSvywRiwcAiXFXA%3D%3D"
       />
     </div>
   );
--- template/Blog.tsx
+++ template/Blog.tsx
@@ -25,7 +25,7 @@ const Blog = (props:any) => {
           {props.description}
         </p>
         <Link
-          href="#"
+          href={props.link}
           className="inline-flex absolute rounded-xl top-50 items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700  hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Read more
